e9318b0a3f13673dcdcbc96ec1e043998c2d7263
Hel1vs <heleen.vansoest@pbl.nl>
2020-12-07 15:39:00 +0100

    add a separate load data section for the waterfalls based on IMAGE native model regions

--- source_waterfall_forAndries.R
+++ source_waterfall_forAndries.R
@@ -2,6 +2,38 @@
 # 1. Run sectoral waterfall script for individual regions
 # 2. Create merged waterfall script showing regions and sectors in one (sectoral breakdown per region)
 
+
+# 0. Load data ------------------------------------------------------------
+setwd("~/disks/y/Project/E555163_COMMIT/Data/Database/Snapshots/Scripts/R/Bridge/Bridge")
+config <- "config_bridge_IMAGE"
+scencateg <- "scen_categ_bridge"
+variables <- "variables_bridge" 
+adjust <- "adjust_reporting_IMAGE"
+addvars <- F
+datafile <-"commit_bridge_image_native_20201207-101452" 
+source("load_data.R") 
+
+# check whether there's only one scenario per category for each model
+check=all[,list(unique(scenario)),by=c("model","Category")]
+View(check) 
+check2=check[,list(length(unique(V1))),by=c("model","Category")]
+View(check2)
+
+# For models with NDCplus, NDCMCS is outdated so remove. For others, keep using NDCMCS until NDCplus is submitted
+check3=check[Category=="NDCplus"]
+View(check3)
+all=all[!c(Category=="NDCMCS"&model%in%unique(check3$model))]
+
+# Load functions and library for plotting
+source("functions/plot_LineNationalScens.R")
+source("functions/plotstyle.R")
+library(grid)
+library(gridExtra)
+
+# fix stupid R mystery
+all$period<-as.numeric(as.character(all$period))
+
+
 # 1. Sectoral waterfalls per region ---------------------------------------
 # Add non-CO2 emissions
 if(!model%in%c("TIAM_Grantham_v3.2","PROMETHEUS")){
@@ -20,7 +52,7 @@ if(!model%in%c("TIAM_Grantham_v3.2","PROMETHEUS")){
 cdata=all
 cdata[model=="AIM/CGE"]$model<-"AIM-CGE"
 
-# loop over models and regions TODO check why it stops after POLES
+# loop over models and regions TODO check why it stops after POLES in the full dataset - but now only running for IMAGE native
 for(i in unique(cdata[Scope=="global"]$model)){
   for(j in unique(cdata[Scope=="global"&!region=="World"]$region)){
     model=i
