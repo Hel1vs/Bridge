5450d02f8b6f73031b269e5c3691603f4c5f15b0
Hel1vs <heleen.vansoest@pbl.nl>
2021-01-22 12:44:22 +0100

    adjustment of bunker emissions reporting for the stocktake tool

--- Bridge_for_stocktake_tool.R
+++ Bridge_for_stocktake_tool.R
@@ -114,11 +114,15 @@ emisc=spread(emisc,region,value)
 emisc[!variable%in%c("Emissions|Kyoto Gases","Emissions|CO2")]$Bunkers <- 0
 emisc=emisc%>%mutate(ROW=World-AUS-BRA-CAN-CHN-EU-IND-IDN-JPN-ROK-RUS-USA-Bunkers) #for now calculating ROW as world minus the shown countries, using the COMMIT countries to be shown
 emisc=data.table(gather(emisc,region,value,c("World","ROW","AUS","BRA","CAN","CHN","EU","IND","IDN","JPN","ROK","RUS","USA","TUR","R5OECD90+EU","R5LAM","R5MAF","R5ASIA","R5REF","Bunkers")))
+bunkers=emisc[region=="Bunkers"&variable=="Emissions|Kyoto Gases"]
+bunkers$variable<-"Emissions|CO2|Excl. AFOLU"
+emisc=emisc[!region=="Bunkers"]
+emisc=rbind(emisc,bunkers)
 emisc=spread(emisc,year,value)
 setcolorder(emisc,c("variable","scenario","region","unit","source","statistic"))
 
 # save
-write.xlsx2(emisc,paste("Stocktaketool","/003v_gst20.xlsx",sep=""),sheetName="data_incl_bunkers",append=T,row.names = F)
+write.xlsx2(emisc,paste("Stocktaketool","/003v_gst20.xlsx",sep=""),sheetName="data_incl_bunkers_CO2",append=T,row.names = F)
 
 
 # Figure 4 - GHG/carbon intensity----------------------------------------------------------------
