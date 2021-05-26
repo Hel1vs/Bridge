418ba372aa09f98b81bc8f64a3931f0acf337fb2
Hel1vs <heleen.vansoest@pbl.nl>
2021-05-26 17:33:16 +0200

    trying to change line style, but not enough types available for clear distinction of 7 different models

--- bridge_results.R
+++ bridge_results.R
@@ -1197,6 +1197,7 @@ F3a = F3a + geom_point(data=range[period %in% c(2030)&Category%in%c("2Deg2030")]
 F3a = F3a + xlim(2010,2052) + scale_y_continuous(breaks=c(-10,0,10,20,30,40,50,60,70,80),limits=c(-15,85))
 F3a = F3a + scale_colour_manual(values=plotstyle(scens))
 F3a = F3a + scale_fill_manual(values=plotstyle(scens))
+F3a = F3a + scale_linetype_manual(values= c("POLES GECO2019" = "dashed","AIM/CGE" = "solid","IMAGE 3.0"= "dotdash","REMIND-MAgPIE 1.7-3.0"= "twodash","WITCH 5.0"= "dotdash","MESSAGEix-GLOBIOM_1.0"= "longdash","COPPE-COFFEE 1.1"= "dotted"))
 F3a = F3a + ylab(paste(unique(all[variable%in%vars]$variable),"[",unique(plotdata$unit),"]"))+ xlab("")
 F3a = F3a + theme_bw() + theme(axis.text.y=element_text(size=20)) + theme(strip.text=element_text(size=14)) + theme(axis.title=element_text(size=20)) +
   theme(axis.text.x = element_text(size=20,angle=90)) + theme(legend.text=element_text(size=14),legend.title=element_blank(),legend.key.width = unit(1,"cm")) #legend.key.size = unit(1.5, "cm"),
